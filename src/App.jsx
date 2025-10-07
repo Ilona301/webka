750cda67c602dfa056a7d3a5744d381a18708b68
Ilonamkrtchyan <127974378+Ilona301@users.noreply.github.com>
2025-10-07 22:26:45 +0400

    header and main section

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css';
+import MainSection from "./components/MainSection.jsx";
+import Header from "./components/Header.jsx";
+
+function App() {
+
+  return (
+    <>
+        <MainSection />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MainSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MainSection = () => {
+    return (
+        <div className="w-full h-[100vh] ">
+            <img
+                src="/webka.png"
+                alt="webka"
+                className="w-full h-full"
+            />
+        </div>
+    );
+};
+
+export default MainSection;
